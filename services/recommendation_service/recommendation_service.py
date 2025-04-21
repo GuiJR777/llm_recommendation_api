fe1c1f40a28a9740e3adee70d2c14248150c450d
Guilherme J Ramires <guilhermejramires@gmail.com>
2025-04-21 16:36:03 -0300

    Refactor RecommendationService to include caching and logging for recommendation generation

--- services/recommendation_service/recommendation_service.py
+++ services/recommendation_service/recommendation_service.py
@@ -7,6 +7,9 @@ from services.recommendation_service.strategies.history_strategy import (
 from services.recommendation_service.strategies.preference_strategy import (
     PreferenceBasedRecommendationStrategy,
 )
+from cache.redis_client import cache_result
+from models.recommendation import Recommendation
+from utils.logger import logger
 
 
 class RecommendationService:
@@ -18,11 +21,17 @@ class RecommendationService:
             "history": HistoryBasedRecommendationStrategy(),
             "preference": PreferenceBasedRecommendationStrategy(),
         }
-
         self.strategy = strategies.get(
             strategy_name, HistoryBasedRecommendationStrategy()
         )
+        logger.info(f"Estratégia definida: {self.strategy.__class__.__name__}")
 
-    def recommend_for_user(self, user_id: str, strategy_name: str = "history"):
+    @cache_result(factory=Recommendation)
+    def recommend_for_user(
+        self, user_id: str, strategy_name: str = "history"
+    ) -> list[Recommendation]:
+        logger.info(
+            f"Gerando recomendações para usuário '{user_id}' com estratégia '{strategy_name}'"  # noqa
+        )
         self.set_strategy(strategy_name)
         return self.strategy.recommend(user_id)
