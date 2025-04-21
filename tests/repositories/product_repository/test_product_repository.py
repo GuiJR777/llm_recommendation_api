abf836be74930d870f34174cdc706852b296f28d
Guilherme J Ramires <guilhermejramires@gmail.com>
2025-04-21 13:11:01 -0300

    Add unit tests for ProductRepository functionality

---
+++ tests/repositories/product_repository/test_product_repository.py
@@ -0,0 +1,27 @@
+from repositories.product_repository import ProductRepository
+from models.product import Product
+
+
+class TestProductRepository:
+    def test_if_product_repository_should_return_product_by_id(self):
+        # Arrange
+        repo = ProductRepository(path="data/products.json")
+
+        # Act
+        product = repo.get_by_id("p1005")
+
+        # Assert
+        assert isinstance(product, Product)
+        assert product.id == "p1005"
+
+    def test_if_product_repository_should_return_product_dict(self):
+        # Arrange
+        repo = ProductRepository(path="data/products.json")
+        product = repo.get_by_id("p1005")
+
+        # Act
+        product_dict = repo.to_dict(product)
+
+        # Assert
+        assert isinstance(product_dict, dict)
+        assert product_dict["id"] == "p1005"
