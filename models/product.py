893e52be8c771bc0bd538f1857a56fcf39762ab3
Guilherme J Ramires <guilhermejramires@gmail.com>
2025-04-21 13:10:23 -0300

    Refactor Product class constructor to include sub_category and average_rating parameters

--- models/product.py
+++ models/product.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import List, Dict
 
 
 class Product:
@@ -6,16 +6,28 @@ class Product:
         self,
         product_id: str,
         name: str,
-        category: str,
-        brand: str,
         description: str,
+        category: str,
+        sub_category: str,
         price: float,
+        brand: str,
+        average_rating: float,
+        num_reviews: int,
+        stock: int,
+        tags: List[str],
+        image_url: str,
         specifications: Dict,
     ):
         self.id = product_id
         self.name = name
-        self.category = category
-        self.brand = brand
         self.description = description
+        self.category = category
+        self.sub_category = sub_category
         self.price = price
+        self.brand = brand
+        self.average_rating = average_rating
+        self.num_reviews = num_reviews
+        self.stock = stock
+        self.tags = tags
+        self.image_url = image_url
         self.specifications = specifications
